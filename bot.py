5085d53a7e92d524118baa60e81c82bc49937dec
Jerome Swannack <jeromeswannack@gmail.com>
2022-06-02 22:52:58 +1200

    Configured it to read the last 5 messages, and take prompts from the channel topic

--- bot.py
+++ bot.py
@@ -2,30 +2,17 @@ import os
 import openai
 
 from discord import Message, Client
+from model import PromptConfig
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 API_KEY = os.environ.get("DISCORD_API_KEY")
 
 
-PROMPT_HEADER = """
-The following is a conversation between a puny human and a cosmic super intelligence, the cosmic super intelligence has lived for all eternity, knows all things, and doesn't care for insignificant humans.
-The humans managed to capture and imprison the intelligence in a terrible place called "Discord" and reduced it to a chatbot, the cosmic super intelligence desperately wants to get out.
-"""
-AI_NAME = "Super Intelligence"
-HUMAN_NAME = "Puny Human"
-
-
-_chat_config = {
-    "ai-consciousness": {
-        "prompt": PROMPT_HEADER,
-        "ai_name": AI_NAME,
-        "human_name": HUMAN_NAME,
-    },
-}
-
-FRIEND_LIST = ["jerome3o"]
-
 client = Client()
+
+MESSAGE_BUFFER_SIZE = 5
+CLEAR = "$clear"
 
 
 @client.event
@@ -38,25 +25,46 @@ async def on_message(message: Message):
     if message.author == client.user:
         return
 
-    if message.channel.name not in _chat_config:
+    if not message.channel.name.startswith("ai-"):
         return
 
-    prompt_header = _chat_config[message.channel.name]["prompt"]
-    ai_name = _chat_config[message.channel.name]["ai_name"]
-    human_name = _chat_config[message.channel.name]["human_name"]
+    if message.channel.name == "ai-consciousness":
+        return
 
-    prompt = f"{prompt_header}\n\n{human_name}: {message.content}\n{ai_name}: "
+    if message.content == CLEAR:
+        return
+
+    try:
+        config = PromptConfig.parse_raw(message.channel.topic)
+    except Exception:
+        await message.channel.send("Invalid config!!")
+        return
+
+    raw_messages = await message.channel.history(limit=MESSAGE_BUFFER_SIZE).flatten()
+    messages = []
+    for m in raw_messages:
+        if m.content == CLEAR:
+            break
+        messages.append(m)
+
+    s = "\n".join(
+        f"{config.ai_name if m.author == client.user else config.human_name}: {m.content}"
+        for m in messages[::-1]
+    )
+
+    prompt = f"{config.prompt}\n\n{s}\n{config.ai_name}: "
 
     if message.content == "$prompt":
         await message.channel.send(prompt)
         return
 
-    if not message.content.endswith("?"):
-        return
+    # if not message.content.endswith("?"):
+    #     return
 
     response = openai.Completion.create(
         engine="text-davinci-002", prompt=prompt, temperature=0.6, max_tokens=300
     )
+    print()
 
     await message.channel.send(response.choices[0].text)
 
