ac6354588b5f2605571685255807edc559641bf8
Jerome Swannack <jeromeswannack@gmail.com>
2022-06-05 17:03:29 +1200

    added context id getter

--- gptbot/bot.py
+++ gptbot/bot.py
@@ -1,15 +1,16 @@
 import os
 import openai
 
-from discord import Message, Client
-from gptbot.legacy.model import PromptConfig
+import discord
+
+from gptbot.helpers import get_context_id_from_message
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 API_KEY = os.environ.get("DISCORD_API_KEY")
 
 
-client = Client()
+client = discord.Client()
 
 MESSAGE_BUFFER_SIZE = 5
 CLEAR = "$clear"
@@ -21,52 +22,14 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message: Message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    if not message.channel.name.startswith("ai-"):
+    if not message.channel.name.startswith("story-ai-"):
         return
 
-    if message.channel.name == "ai-consciousness":
-        return
-
-    if message.content == CLEAR:
-        return
-
-    try:
-        config = PromptConfig.parse_raw(message.channel.topic)
-    except Exception:
-        await message.channel.send("Invalid config!!")
-        return
-
-    raw_messages = await message.channel.history(limit=MESSAGE_BUFFER_SIZE).flatten()
-    messages = []
-    for m in raw_messages:
-        if m.content == CLEAR:
-            break
-        messages.append(m)
-
-    s = "\n".join(
-        f"{config.ai_name if m.author == client.user else config.human_name}: {m.content}"
-        for m in messages[::-1]
-    )
-
-    prompt = f"{config.prompt}\n\n{s}\n{config.ai_name}: "
-
-    if message.content == "$prompt":
-        await message.channel.send(prompt)
-        return
-
-    # if not message.content.endswith("?"):
-    #     return
-
-    response = openai.Completion.create(
-        engine="text-davinci-002", prompt=prompt, temperature=0.6, max_tokens=300
-    )
-    print()
-
-    await message.channel.send(response.choices[0].text)
+    context_id = get_context_id_from_message(message)
 
 
 client.run(API_KEY)
--- gptbot/helpers.py
+++ gptbot/helpers.py
@@ -4,6 +4,10 @@ import discord
 def get_context_id_from_message(message: discord.Message):
     channel: discord.TextChannel = message.channel
     channel_name = channel.name
+    guild_name = message.guild.name
+    guild_id = message.guild.id
+
+    return f"{channel_name}-{guild_name}-{guild_id}"
 
     # TODO: get identifier for discord server
     # server_name = channel.
